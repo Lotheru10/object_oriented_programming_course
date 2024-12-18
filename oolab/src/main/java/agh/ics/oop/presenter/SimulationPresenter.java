c6a006bad56aeaa2b3f0cf3b5112509277b43adb
Lotheru10 <jakubprygiel72@gmail.com>
2024-12-18 23:24:52 +0100

    lab8 something

---
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -0,0 +1,22 @@
+package agh.ics.oop.presenter;
+
+
+import agh.ics.oop.model.*;
+import javafx.application.Platform;
+
+public class SimulationPresenter implements MapChangeListener{
+    private WorldMap map;
+
+    public void setWorldMap(WorldMap map){
+        this.map = map;
+    }
+    public void drawMap(){
+
+    }
+    @Override
+    public void mapChanged(WorldMap worldMap, String message) {
+        setWorldMap(worldMap);
+        drawMap();
+        };
+    }
+
